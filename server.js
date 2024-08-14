4f3de54f1685f61463548e5e804c77ea34c989bf
ranaprabal <ranaprabal00@gmail.com>
2024-08-15 03:51:26 +0530

    final commit

--- server.js
+++ server.js
@@ -1,14 +1,41 @@
-const express = require("express")
 require("dotenv").config()
-
+const cookieParser = require("cookie-parser")
+const express = require("express")
 const app = express()
 
+app.use(cookieParser())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+const { testDbConnection, sq } = require("./config/database")
+testDbConnection()
+
+require("./models/associations")
+
+sq.sync({ force: false })
+  .then(() => {
+    console.log("Database & tables created!")
+  })
+  .catch((err) => {
+    console.error("Error creating database & tables", err)
+  })
+
 const port = process.env.PORT || 3000
+
+const authRoutes = require("./routes/authRoutes")
+const courseRoutes = require("./routes/courseRoutes")
+const leadRoutes = require("./routes/leadRoutes")
+const commentRoutes = require("./routes/commentRoutes")
+
+app.use("/api/auth", authRoutes)
+app.use("/api/course", courseRoutes)
+app.use("/api/lead", leadRoutes)
+app.use("/api/comment", commentRoutes)
 
 app.get("/", (req, res) => {
   res.send("Hello World!")
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`App listening on port ${port}`)
 })
